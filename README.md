c1a8b8c992baf0ead04de77c4d3ae50f57402ee6
schefbi <41326409+schefbi@users.noreply.github.com>
2023-12-15 15:56:20 +0000

    Deploying to gh-pages from @ bkd-mba-fbi/event-information@065ff8cd0555e3adc88d547eae6ce5c37bc5252d 🚀

---
+++ README.md
@@ -0,0 +1,2 @@
+# event-information
+Display events from evento api to add this to entrance of a buildung or of a room.
---
+++ settings.js
@@ -0,0 +1,11 @@
+window.eventInformation = window.eventInformation || {};
+window.eventInformation.settings = {
+
+    //OAuth Url Evento
+    authUrl: "https://eventoapp.apps.be.ch/OAuth",
+    // Rest API Url Evento
+    restUrl: "https://eventoapp.apps.be.ch/restApi",
+    //ClientId form Public Consumer
+    clientId: "eventInfo",
+
+};
